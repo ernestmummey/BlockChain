106487d3b37c7bf12c5688ce73679253a1ce24a4
ernest_mummey <69600478+ernestmummey@users.noreply.github.com>
2021-09-24 22:06:02 -0400

    Starting the blockchain project

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask  
+app = Flask(__name__)  
+
+
+
+@app.route('/')          
+def hello_world():
+    return 'Hello World!' 
+
+
+# import hashlib
+
+#encoded = string.encode()
+
+# class Block:
+#     def __init__(self, index, timestamp, sender, receiver, amount, previousHash = ''):
+#         self.index = index
+#         self.timestamp = timestamp
+#         self.sender = sender
+#         self.receiver = receiver
+#         self.amount = amount
+#         self.previousHash = previousHash
+#         self.hash = ''
+
+#         def calculateHash():
+#             return hashlib.sha256(index + previousHash + timestamp + sender + reciever + )
+#             pass
+if __name__=="__main__":      
+    app.run(debug=True) 
